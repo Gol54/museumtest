8ad2a6f6f45738f08958ac9cb20c32ed0ac411b8
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2022-11-14 17:47:01 +0300

    порядок

--- app/js/test.js
+++ app/js/test.js
@@ -1,3 +1,4 @@
+/*
 let today = new Date();
 let hourHow = today.getHours();
 let getting;
@@ -17,5 +18,26 @@ if(hourHow<=24 && hourHow>=18){
     document.write('<h3>'+ morning +'</h3>');
 }
 if(hourHow<6 && hourHow>0){
-    document.write('<h3>'+ morning +'</h3>');
+    document.write('<h3>'+ morning +'</h3>')
 }
+*/
+
+let _helloText = 'Hellow';
+let _name = 'romaLox';
+let _massage = 'Please check your prise!';
+let _ban = _helloText +' '+ _name +' '+ _massage
+let _priseChar = 5;
+
+let _costBan =_ban.length;
+let _cost = _priseChar * _costBan
+
+let n = _ban + ' ' + 'cost: ' + _costBan;
+
+let _welcomeText = document.getElementById("WelcomeText");
+_welcomeText.textContent = _ban + '!';
+
+let _welcomeText2 = document.getElementById("WelcomeText2");
+_welcomeText2.textContent = _cost;
+
+
+alert(n);
